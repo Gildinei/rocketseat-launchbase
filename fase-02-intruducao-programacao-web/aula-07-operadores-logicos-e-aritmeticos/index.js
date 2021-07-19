49579c8a25e5e7815ce49d22eb7c13e272e7a024
Gildinei S. Anunciação <gildineisa@gmail.com>
2021-07-19 20:00:23 -0300

    Aula 07

---
+++ fase-02-intruducao-programacao-web/aula-07-operadores-logicos-e-aritmeticos/index.js
@@ -0,0 +1,15 @@
+console.log(5 == 5 && 6 == 6); // true;
+console.log(5 == 5 && 6 == 7); // false;
+console.log(5 == 4 && 6 == 7); // false;
+
+console.log(5 == 5 || 6 == 6); // true
+console.log(5 == 5 || 6 == 7); // true
+console.log(5 == 4 || 6 == 7); // false
+
+console.log((5 == 5) && !(6 == 7)); // true
+
+console.log(5 + 5);
+console.log(5 - 5);
+console.log(5 * 5);
+console.log(5 / 5);
+console.log(5 % 5);
