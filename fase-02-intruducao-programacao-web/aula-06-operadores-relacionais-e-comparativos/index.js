82882435c8d00b0d5a22888063c997af4c85c1e6
Gildinei Santos Anunciação <gildineisa@gmail.com>
2021-07-19 16:30:30 -0300

    Aula 06

---
+++ fase-02-intruducao-programacao-web/aula-06-operadores-relacionais-e-comparativos/index.js
@@ -0,0 +1,24 @@
+
+console.log(5 == 4)
+
+console.log(5 === 4)
+
+console.log(5 != 4)
+
+console.log(5 !== 4)
+
+console.log(5 > 4)
+
+console.log(5 < 4)
+
+console.log(5 <= 4)
+
+console.log(5 >= 4)
+
+const idade = 16;
+
+if (idade >= 18) {
+    console.log(`${idade} anos, pode entrar.`);
+}else {
+    console.log(`${idade} anos, não pode emtrar.`);
+}
