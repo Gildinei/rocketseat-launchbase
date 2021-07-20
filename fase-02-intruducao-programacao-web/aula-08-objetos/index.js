42625b919793e7691f9a0c310e1be71977cd45bd
Gildinei S. Anunciação <gildineisa@gmail.com>
2021-07-19 21:25:52 -0300

    Aula 08

---
+++ fase-02-intruducao-programacao-web/aula-08-objetos/index.js
@@ -0,0 +1,16 @@
+const aluno01 = {
+    nome: "Gildinei",
+    nota: 9.8
+};
+
+const aluno02 = {
+    nome: "Mayk",
+    nota: 10
+};
+
+const aluno03 = {
+    nome: "Diego",
+    nota: 2
+};
+
+const media = (aluno01.nota + aluno02.nota + aluno03.nota) / 3;
