4fb216b0d720d356ae1991248bbf55b3003c5672
Gildinei Santos Anunciação <gildineisa@gmail.com>
2021-07-20 19:57:42 -0300

    Desafio 01-04

---
+++ fase-02-intruducao-programacao-web/desafios/operacoes-bancarias.js
@@ -0,0 +1,30 @@
+const user = {
+    name: "Mariana",
+    transactions: [],
+    balance: 0
+};
+
+function createTransaction(transaction) {
+    user.transactions.push(transaction);
+
+    if (transaction.type === 'credit') {
+        user.balance += transaction.value;
+    } else {
+        user.balance -= transaction.value;
+    };
+};
+
+createTransaction({ type: "credit", value: 50 });
+createTransaction({ type: "credit", value: 120 });
+
+createTransaction({ type: "debit", value: 80 });
+createTransaction({ type: "debit", value: 30 });
+
+console.log(user.balance); // 60
+
+// getHigherTransactionByType("credit"); // { type: 'credit', value: 120 }
+// getHigherTransactionByType("debit"); // { type: 'debit', value: 80 }
+
+// getAverageTransactionValue(); // 70
+
+// getTransactionsCount(); // { credit: 2, debit: 2 }
