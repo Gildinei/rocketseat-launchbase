1eddca6d8439f5f6844a4f7e7b85a9ebf6c7f874
Gildinei Santos Anunciação <gildineisa@gmail.com>
2021-07-29 14:54:16 -0300

    Aula 03: Criando rotas

--- fase-02-intruducao-programacao-web/iniciando-no-back-end/server.js
+++ fase-02-intruducao-programacao-web/iniciando-no-back-end/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const server = express();
 
-server.listen(5000, function() {
+server.get('/', function (request, response) {
+    return response.send("Hi!");
+});
+
+server.listen(5000, function () {
     console.log(`Server is running...`);
 });
